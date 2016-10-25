22e25a169f428329e648f5abb3166a559d0bd421
Paddy <paddy@carvers.co>
2016-10-24 21:15:54 -0700

    Use subcommands instead of flags.
    Trade out the flags package in the standard library for os.Args and
    subcommands. This requires us to write our own usage message.

--- prov-stdlib/main.go
+++ prov-stdlib/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"net/http"
 	"os"
@@ -10,7 +9,40 @@ import (
 	"github.com/paddyforan/go-cli-talk/proverbs"
 )
 
+func printUsage() {
+	yellow := color.New(color.FgYellow)
+	printYellow := yellow.SprintFunc()
+	fmt.Printf("%s prov-stdlib COMMAND [ID]\n\nSupported commands:\n\trandom\t\tReturn a random proverb.\n\tget $ID\t\tReturn the proverb associated with $ID.\n\thelp\t\tPrint this message.\n", printYellow("Usage:"))
+}
+
 func main() {
+	// let's use subcommands
+	var action, id string
+	if len(os.Args) < 2 || len(os.Args) > 3 {
+		printUsage()
+		os.Exit(1)
+	}
+	action = os.Args[1]
+	if len(os.Args) > 2 {
+		id = os.Args[2]
+	}
+	if action == "get" && id == "" {
+		printUsage()
+		os.Exit(1)
+	}
+	if action == "random" && id != "" {
+		printUsage()
+		os.Exit(1)
+	}
+	if action == "help" {
+		printUsage()
+		return
+	}
+	if action != "help" && action != "get" && action != "random" {
+		printUsage()
+		os.Exit(1)
+	}
+
 	// let's add some color
 	red := color.New(color.FgRed)
 	boldRed := red.Add(color.Bold)
@@ -22,11 +54,6 @@ func main() {
 		fmt.Printf("%s PROVERBS_URL must be set to the API endpoint to retrieve proverbs from.\n", printBoldRed("[ERROR]"))
 		os.Exit(1) // exit codes are a simple call
 	}
-
-	// we can also read command line arguments
-	var id string
-	flag.StringVar(&id, "proverb", "", "the ID of the proverb to retrieve; leave empty for a random proverb")
-	flag.Parse()
 
 	headers := http.Header{}
 
